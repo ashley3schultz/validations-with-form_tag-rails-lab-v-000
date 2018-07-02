86891c21f766ec57e9b01909275e652e2a5c3638
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-02 00:31:30 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,9 +10,8 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    if Post.new(post_params).valid?
-      @post.submit(post_params)
-      #.update(post_params)
+    if @post.submit(post_params)
+      #@post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
